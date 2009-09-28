3f9f904e29ba1ca49171dac4a2383b7fe724a3bb
Matthias Viehweger <kronn@kronn.de>
2009-09-28 16:18:48 +0200

    Tests for quoted primary_key-names and changed primary_key-name

--- test/mysql_adapter_test.rb
+++ test/mysql_adapter_test.rb
@@ -5,35 +5,42 @@ class MysqlAdapterTest < ActiveRecord::TestCase
 
   def test_add_without_options
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(id)",
+      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(`id`)",
       add_foreign_key(:employees, :companies)
     )
   end
   
   def test_add_with_name
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `favorite_company_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(id)",
+      "ALTER TABLE `employees` ADD CONSTRAINT `favorite_company_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(`id`)",
       add_foreign_key(:employees, :companies, :name => 'favorite_company_fk')
     )
   end
   
   def test_add_with_column
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `employees_last_employer_id_fk` FOREIGN KEY (`last_employer_id`) REFERENCES `companies`(id)",
+      "ALTER TABLE `employees` ADD CONSTRAINT `employees_last_employer_id_fk` FOREIGN KEY (`last_employer_id`) REFERENCES `companies`(`id`)",
       add_foreign_key(:employees, :companies, :column => 'last_employer_id')
     ) 
+  end
+
+  def test_add_with_primary_key
+    assert_equal(
+      "ALTER TABLE `employees` ADD CONSTRAINT `employees_last_employer_id_fk` FOREIGN KEY (`last_employer_id`) REFERENCES `companies`(`employer_id`)",
+      add_foreign_key(:employees, :companies, :primary_key => 'employer_id')
+    )
   end
   
   def test_add_with_column_and_name
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `favorite_company_fk` FOREIGN KEY (`last_employer_id`) REFERENCES `companies`(id)",
+      "ALTER TABLE `employees` ADD CONSTRAINT `favorite_company_fk` FOREIGN KEY (`last_employer_id`) REFERENCES `companies`(`id`)",
       add_foreign_key(:employees, :companies, :column => 'last_employer_id', :name => 'favorite_company_fk')
     )
   end
   
   def test_add_with_delete_dependency
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(id) " +
+      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(`id`) " +
       "ON DELETE CASCADE",
       add_foreign_key(:employees, :companies, :dependent => :delete)
     )
@@ -41,7 +48,7 @@ class MysqlAdapterTest < ActiveRecord::TestCase
   
   def test_add_with_nullify_dependency
     assert_equal(
-      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(id) " +
+      "ALTER TABLE `employees` ADD CONSTRAINT `employees_company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `companies`(`id`) " +
       "ON DELETE SET NULL",
       add_foreign_key(:employees, :companies, :dependent => :nullify)
     )
